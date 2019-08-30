84292555eb4fa671cbdfef02fe9c0e14ac785385
lengyaohui <lengyaohui.bj@acewill.cn>
2019-08-30 11:42:03 +0800

    md

--- temp.py
+++ temp.py
@@ -7,3 +7,16 @@ data = {
 
 req_str = MultipartFormData.to_form_data(data)
 print(req_str)
+
+# result
+"""
+------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="id"
+
+12313123
+------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="name"
+
+yhleng
+------WebKitFormBoundary7MA4YWxkTrZu0gW--
+"""
